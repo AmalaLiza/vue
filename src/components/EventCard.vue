9d9a25bccf1a1527ed0a24a16287752391652224
Amala James <94124307+amala-paystack@users.noreply.github.com>
2021-11-19 14:09:21 +0400

    Initial commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return 'I am amala'
+  },
+  props: {
+    props: {
+      type: Number,
+      default: 1,
+    },
+  },
+}
+</script>
+
+<style></style>
+ 
