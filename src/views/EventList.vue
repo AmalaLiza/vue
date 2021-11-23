d1bf77a353483727cb59c4cfab49caf02b224a8d
Amala James <94124307+amala-paystack@users.noreply.github.com>
2021-11-23 11:36:13 +0400

    Added Event Services

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="about">
     <h1>Event List page</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
     <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
     EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   },
 }
 </script>
